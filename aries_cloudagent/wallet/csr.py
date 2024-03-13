e2c10e3d65fe664117bc0e83d7e72a1b1f698c75
Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>
2024-03-13 15:29:41 -0400

    refactoring of csr function
    Signed-off-by: Julio Cesar Torres dos Santos <julio.cesartorres@mcn.gouv.qc.ca>

--- aries_cloudagent/wallet/csr.py
+++ aries_cloudagent/wallet/csr.py
@@ -5,8 +5,14 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def create_csr( common_name, country=None, state=None, city=None, 
-                organization=None, organizational_unit=None, email=None, key=None):
+def create_csr( common_name, 
+                country="CA", 
+                state=None, 
+                city=None, 
+                organization=None, 
+                organizational_unit=None, 
+                email=None, 
+                key=None):
     """
     Creates a Certificate Signing Request (CSR) with the provided details.
 
@@ -14,36 +20,41 @@ def create_csr( common_name, country=None, state=None, city=None,
     country, state, city, organization, organizational unit, and email.
 
     Args:
-        common_name (str): The common name for the CSR (e.g., the fully-qualified domain name). This attribute is required.
-        country (str, optional): The two-letter ISO code for the country. Defaults to None.
-        state (str, optional): The full name of the state or province. Defaults to None.
-        city (str, optional): The name of the city. Defaults to None.
-        organization (str, optional): The name of the organization. Defaults to None.
-        organizational_unit (str, optional): The name of the organizational unit. Defaults to None.
-        email (str, optional): The email address. Defaults to None.
-        key (cryptography.hazmat.primitives.asymmetric.ec): The private key to sign the CSR. Defaults to None. This attribute is required.
+        common_name (str)                                   : The common name for the CSR (e.g., the fully-qualified domain name). This attribute is required.
+        country (str, optional)                             : The two-letter ISO code for the country. Defaults to None.
+        state (str, optional)                               : The full name of the state or province. Defaults to None.
+        city (str, optional)                                : The name of the city. Defaults to None.
+        organization (str, optional)                        : The name of the organization. Defaults to None.
+        organizational_unit (str, optional)                 : The name of the organizational unit. Defaults to None.
+        email (str, optional)                               : The email address. Defaults to None.
+        key (cryptography.hazmat.primitives.asymmetric.ec)  : The private key to sign the CSR. Defaults to None. This attribute is required.
     Returns:
-        x509.CertificateSigningRequest: The generated CSR.
+        x509.CertificateSigningRequest                      : The generated CSR.
 
     Raises:
-        ValueError: If the common_name is not a valid identification name, domain name or IP address.
+        ValueError                                          : If the common_name is not a valid identification name, domain name or IP address.
     """
 
     if not common_name:
-        raise ValueError("common_name is required")
+        raise ValueError("A Common Name is required")
     
-    # Provide various details about who we are.Create the optional attributes for the CSR 
-    # if they are provided. At the end, add the mandatory common name.
+    # Provide various details about who we are, to build a distinguished name fields.
+    # Create the optional attributes for the CSR if they are provided. 
+    # At the end, add the mandatory common name.
     attributes = []
 
     if country:
         attributes.append(x509.NameAttribute(NameOID.COUNTRY_NAME, country))
+
     if state:
         attributes.append(x509.NameAttribute(NameOID.STATE_OR_PROVINCE_NAME, state))
+
     if city:
         attributes.append(x509.NameAttribute(NameOID.LOCALITY_NAME, city))
+
     if organization:
         attributes.append(x509.NameAttribute(NameOID.ORGANIZATION_NAME, organization))
+
     if organizational_unit:
         attributes.append(x509.NameAttribute(NameOID.ORGANIZATIONAL_UNIT_NAME, organizational_unit))
 
@@ -56,26 +67,13 @@ def create_csr( common_name, country=None, state=None, city=None,
     if email:
         extensions.append(x509.RFC822Name(email))
 
+    # Create the CSR builder with the provided details.
+    csr_builder = x509.CertificateSigningRequestBuilder().subject_name(x509.Name(attributes))
 
-    # Create the CSR with the provided details.
-    csr = x509.CertificateSigningRequestBuilder().subject_name(x509.Name(
+    # Add the extensions to the CSR if they are provided.
+    if len(extensions) > 0:
+        csr_builder = csr_builder.add_extension(x509.SubjectAlternativeName(extensions), critical=False)
 
-        # Identification attributes
-        attributes
+    csr_builder = csr_builder.sign(key, hashes.SHA256())
 
-    )).add_extension(
-
-        x509.SubjectAlternativeName(
-
-            # Email 
-            extensions
-
-        ),
-
-        critical=False,
-
-    # Sign the CSR with our private key.
-
-    ).sign(key, hashes.SHA256())
-
-    return csr
+    return csr_builder
